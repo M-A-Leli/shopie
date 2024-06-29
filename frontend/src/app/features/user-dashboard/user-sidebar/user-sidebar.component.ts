44e1f437f01461cff52dcd848a3b9558f783483f
M-A-Leli <lelialvin23@gmail.com>
2024-06-29 14:03:12 +0300

    Initial user dashboard ui

--- frontend/src/app/features/user-dashboard/user-sidebar/user-sidebar.component.ts
+++ frontend/src/app/features/user-dashboard/user-sidebar/user-sidebar.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user-sidebar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-sidebar.component.html',
   styleUrl: './user-sidebar.component.css'
 })
 export class UserSidebarComponent {
+  isCollapsed = false;
 
+  toggleSidebar(): void {
+    this.isCollapsed = !this.isCollapsed;
+  }
 }
