125c9ea8d8844a66b26ec788c7fc6bbda27535ac
Naomi2512 <naomichege2512@gmail.com>
2024-07-03 11:55:43 +0300

    added view one product functionality

--- frontend/src/app/shared/models/Product.ts
+++ frontend/src/app/shared/models/Product.ts
@@ -4,14 +4,14 @@ import ProductImage from "./ProductImage";
 import Review from "./Review";
 
 export default interface Product {
-  id?: string,
+  id: string,
   name: string,
   description: string,
   price: number,
   stock_quantity: number,
   category_id: string,
   category?: Category, //!
-  images?: ProductImage[], //!
+  images: ProductImage[], //!
   reviews?: Review[], //!
   cart_items?: CartItem[] //!
 }
